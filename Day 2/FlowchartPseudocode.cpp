1313174514d5f7c8202396cdd030c14316a6f255
Sayantan699 <sayantanhalder43@gmail.com>
2025-12-05 21:01:14 +0530

    first commit

---
+++ Day 2/FlowchartPseudocode.cpp
@@ -0,0 +1,37 @@
+//Flowchart -> Systematic representation of the flow of algorithms
+
+//Pseudocode -> Pseudocode is a detailed yet readable description of what a computer program or algorithm must do, expressed in a formally-styled natural language rather than in a programming language.
+
+//Flowchart and Pseudocode are used for planning and visualizing algorithms before actual coding.
+
+//Example: Flowchart and Pseudocode for finding the maximum of two numbers
+/*
+Flowchart:
+ Start
+   |
+   v
+ Input A, B
+   |
+   v
+ Is A > B?
+  / \
+    /   \
+    Yes   No
+    |      |
+    v      v
+ Print A  Print B
+    |
+    v
+      End
+Pseudocode:
+
+    Start
+    Input A, B
+    If A > B Then
+        Print A
+    Else
+        Print B
+    EndIf
+    End
+    */
+   
