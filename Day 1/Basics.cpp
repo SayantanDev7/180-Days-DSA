ade667e5c32c3f201ed6d9b5f74c52ad4d522ce3
Sayantan699 <sayantanhalder43@gmail.com>
2025-12-05 18:51:27 +0530

    first commit

---
+++ Day 1/Basics.cpp
@@ -0,0 +1,11 @@
+//Binary No
+
+//What are Binary Numbers?
+// A binary number is a number expressed in the base-2 numeral system which uses only two digits: 0 and 1.
+
+// Decimal to Binary Conversion
+
+// Convert a given decimal number to its binary representation by dividing the number by 2 repeatedly and storing the remainders.
+
+//According to Moorey Transistor capacity is getting doubled every 2 years. 
+//So to store large data we need to use binary numbers 
